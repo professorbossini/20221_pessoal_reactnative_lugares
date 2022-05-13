a1eaaf92b9ab2263df3c98fb819e3a093a662d59
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-13 17:40:27 -0300

    apostila07-app-recursos-nativos: fim da listagem 2.3.4
    componente principal devolve a JSX que representa a estrutura de navegacao entre telas

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View>
-      <Text>App</Text>      
-    </View>
-  );
-}
+import container from './navegacao/LugaresNavigator'
 
-const styles = StyleSheet.create({
-  
-});
+export default function App() {
+  return container
+}
